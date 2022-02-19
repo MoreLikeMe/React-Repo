b91a2b2f46b591b350f650b4d89fd852969e0874
@MoreLikeMe <sharmajoy96@gmail.com>
2022-02-20 00:22:51 +0530

    todo-list-delete-order-not-working

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,9 +1,53 @@
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+  const [toDoList, setToDoList] = useState([]);
+  const [item, setItem] = useState("");
+
+  const updateCurrentItem = (e) => {
+    setItem(e.target.value)
+  }
+
+  const addToList = () => {
+    const tempList = [...toDoList];
+    tempList.push({key: Math.random(),"itemValue" :item});
+    setToDoList(tempList);
+    console.log(toDoList);
+  }
+
+  const deleteFromList = (currItem) => {
+    let tempList = [...toDoList];
+    tempList = tempList.filter((cItem) => cItem.key!==currItem.key);
+    console.log(tempList);
+    setToDoList(tempList);
+  }
+
   return (
-    <div className="App">
-      Hello
+    <div>
+      Search Bar:
+      <div className="mb-3">
+        <label for="search" className="form-label"></label>
+        <input onChange={(e) => updateCurrentItem(e)} value={item} type="text" className="form-control" name="search" id="" placeholder="Type Here...  " />
+      </div>
+
+      <div className="d-grid gap-2"> 
+        <button type="button" name="search-but" id="" className="btn btn-primary" onClick={addToList}>
+          Add
+        </button>
+      </div>
+
+      <div>
+        <ul>
+          {toDoList.map( curItem => (
+            <li>{curItem.itemValue}
+             <img src="Red_Cross.png" alt="" onClick={() => deleteFromList({curItem})}/>
+            </li>
+          ))}
+        </ul>
+      </div>
+
     </div>
   );
 }
